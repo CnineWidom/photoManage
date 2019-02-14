0ff441c132224bf42f0436dffe3687eafa320286
yy87945120 <594115018@qq.com>
2019-02-14 20:13:12 +0800

    【更新】staticweb
    修改footer样式
    增加了对移动端的适应

--- resources/views/staticweb/pic/pc/js/index.js
+++ resources/views/staticweb/pic/pc/js/index.js
@@ -1,4 +1,22 @@
 // 公共函数开始
+// 缩放屏幕
+var phoneWidth = parseInt(window.screen.width);
+            var phoneHeight = parseInt(window.screen.height);
+            var phoneScale = phoneWidth/1100;//除以的值按手机的物理分辨率
+            var ua = navigator.userAgent;
+            if (/Android (\d+\.\d+)/.test(ua)) {
+            var version = parseFloat(RegExp.$1);
+            // andriod 2.3
+            if (version > 2.3) {
+            document.write('<meta name="viewport" content="width=device-width,initial-scale='+phoneScale+',minimum-scale='+phoneScale+',maximum-scale='+phoneScale+',user-scalable=yes">');
+            // andriod 2.3以上
+            } else {
+            document.write('<meta name="viewport" content="width=device-width,user-scalable=yes">');
+            }
+            // 其他系统
+            } else {
+            document.write('<meta name="viewport" content="width=device-width, initial-scale='+phoneScale+',minimum-scale='+phoneScale+',maximum-scale ='+phoneScale+',user-scalable=yes">');
+            }
 // 后台地址
 var ajaxUrl = "";
 // 异步ajax
