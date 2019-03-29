4e619b63fba3507f8c04ef9e0d503feb7ff70761
CnineWidom <2779386157@qq.com>
2019-03-29 19:14:16 +0800

    上传图片

--- app/Http/Controllers/uploadController.php
+++ app/Http/Controllers/uploadController.php
@@ -2,7 +2,7 @@
 /*图片上传*/
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 use App\Http\Requests\createRequest;
 class uploadController extends Controller
 {
@@ -17,11 +17,22 @@ class uploadController extends Controller
 		return view('web.pic.pc.uploadPicture');
 	}
 
-	public function doupload(createRequest $request)
-	{
-		 $title = $request->get('title');
-		 $content = $request->get('content');
-		 $file = $request->get('files');
-		 var_dump($file);
-	}
+//	public function doupload(createRequest $request)
+//	{
+////		 $title = $request->get('title');
+////		 $content = $request->get('content');
+////		 $file = $request->file();
+//		 $res = $request->all();
+//		 var_dump($res);
+//	}
+    public function doupload(createRequest $request)
+    {
+//        header("Content-Type: text/html; charset=utf-8");
+//		 $title = $request->get('title');
+//		 $content = $request->get('content');
+//		 $file = $request->file();
+        $data = $request->all();
+        $res = $request->file('files');
+        dd($data);
+    }
 }
