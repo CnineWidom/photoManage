c6e3456f265dd9389efe98bdc42853bed2edaefa
huifeidezhu <2779386157@qq.com>
2019-04-17 22:29:44 +0800

    还是

--- app/Http/Controllers/uploadController.php
+++ app/Http/Controllers/uploadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\createRequest;
+use Intervention\Image\ImageManager;
 
 class uploadController extends Controller
 {
@@ -38,15 +39,69 @@ class uploadController extends Controller
 		return view('web.pic.pc.uploadPicture',$data);
 	}
 
-	public function doupload(createRequest $request)
+	public function doupload(createRequest $request,ImageManager $image)
 	{
 		$res= $request->all();
 		$file = $request->file('file');
-		dd($res);
-		echo json_encode($res['title']);
+		if(!empty($file)){
+			$tmpFileName = $file->getPathname();
+            $path = public_path();
+            $name = md5(base64_decode(time()));
+            $newFileName = $this->newFileName.$name.$this->markBack;
+            $newFileNameByTmp = $this->newFileNameByTmp.$name.$this->markBack;
+
+            $img = $image->make($tmpFileName)->resize(300,300);
+            $img->save($path.$newFileName);
+            if($this->useWalkMark){
+                $img->text($this->markText,140,140,function ($font){
+                    $font->file('C:/Windows/Fonts/STXINWEI.TTF');//使用本地ttf文件 使用laravel自带的话会出现中文乱码
+//                    $font->file(2);
+                    $font->size(40);
+                    $font->color('#fff');
+                    $font->align('center');
+                });
+            }
+            else{
+                $img->insert($this->markPicPath);
+            }
+
+            $img->save($path.$newFileNameByTmp);
+		}
+		// dd($res);
+		// echo json_encode($res['title']);
 		 // $title = $request->get('title');
 		 // $content = $request->get('content');
 		 // $file = $request->all();
 		 // dd($file);
+	}
+
+	public function saveImage($image,$newFileName,$newFileNameByTmp,$path)
+	{
+		$img = $image->make($tmpFileName)->resize(300,300);
+        $img->save($path.$newFileName);
+        if($this->useWalkMark){
+            $img->text($this->markText,140,140,function ($font){
+                $font->file('C:/Windows/Fonts/STXINWEI.TTF');//使用本地ttf文件 使用laravel自带的话会出现中文乱码
+//                    $font->file(2);
+                $font->size(40);
+                $font->color('#fff');
+                $font->align('center');
+            });
+        }
+        else{
+            $img->insert($this->markPicPath);
+        }
+        $img->save($path.$newFileNameByTmp);
+
+        $imgReplacePath = $path.$imageRes->image_load;
+        $imgReplacePathTmp = str_replace('user','userTmp',$imgReplacePath);
+        if(file_exists($imgReplacePath))
+        {
+            unlink($imgReplacePath);
+            unlink($imgReplacePathTmp);
+        }
+        $data['image_load'] = $newFileName;
+        $res['image'] = $newFileName;
+        return true;
 	}
 }
